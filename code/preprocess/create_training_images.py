0413232185937504ad7e98e1bb09a53eb25b7caa
lmosser <lukas.mosser@gmail.com>
2017-04-08 21:19:59 +0200

    Added preprocessing script

---
+++ code/preprocess/create_training_images.py
@@ -0,0 +1,43 @@
+import tifffile
+import numpy as np
+import h5py
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', required=True, help='3D')
+parser.add_argument('--image', required=True, help='path to image')
+parser.add_argument('--name', required=True, help='name of dataset')
+parser.add_argument('--edgelength', type=int, default=128, help='input batch size')
+parser.add_argument('--stride', type=int, default=32, help='the height / width of the input image to network')
+parser.add_argument('--target_dir', required=True, help='path to store training images')
+
+opt = parser.parse_args()
+print(opt)
+
+img = tifffile.imread(str(opt.image))
+
+count = 0
+
+edge_length = opt.edgelength #image dimensions
+stride = opt.stride #stride at which images are extracted
+
+N = edge_length
+M = edge_length
+O = edge_length
+
+I_inc = stride
+J_inc = stride
+K_inc = stride
+
+target_direc = str(opt.target_dir)
+
+for i in range(0, img.shape[0], I_inc):
+    for j in range(0, img.shape[1], J_inc):
+        for k in range(0, img.shape[2], K_inc):
+            subset = img[i:i+N, j:j+N, k:k+O]
+            if subset.shape == (N, M, O):
+                f = h5py.File(target_direc+"/"+str(opt.name)+"_"+str(count)+".hdf5, "w")
+                f.create_dataset('data', data=subset, dtype="i8", compression="gzip")
+                f.close()
+                count += 1
+print count
